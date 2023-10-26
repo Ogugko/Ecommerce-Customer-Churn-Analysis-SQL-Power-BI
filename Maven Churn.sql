eb091f0a7a13d06fd275490e2069703b561bfae6
Kenneth O <143842831+Ogugko@users.noreply.github.com>
2023-10-26 15:22:07 +0100

    Maven Churn.sql

---
+++ Maven Churn.sql
@@ -0,0 +1,154 @@
+/****************
+DATA EXPLORATION
+*****************/
+
+-- 1. How much revenue did Maven lose to churned customers?
+SELECT Customer_Status, COUNT(Customer_ID) AS customer_count,ROUND((SUM(Total_Revenue) * 100.0) / SUM(SUM(Total_Revenue)) OVER(), 1) AS Revenue_Percentage 
+FROM churn
+GROUP BY Customer_Status; 
+
+-- 2. Which cities have the higest churn rates?
+SELECT City, COUNT(Customer_ID) AS Churned, CEILING(COUNT(CASE WHEN Customer_Status = 'Churned' THEN Customer_ID ELSE NULL END) * 100.0 / COUNT(Customer_ID)) AS Churn_Rate
+FROM churn
+GROUP BY City
+HAVING COUNT(Customer_ID)  > 30
+AND COUNT(CASE WHEN Customer_Status = 'Churned' THEN Customer_ID ELSE NULL END) > 0
+ORDER BY Churn_Rate DESC
+LIMIT 4;
+
+-- 3. Why did churners leave and how much did it cost?
+SELECT Churn_Category, ROUND(SUM(Total_Revenue),0)AS Churned_Rev, CEILING((COUNT(Customer_ID) * 100.0) / SUM(COUNT(Customer_ID)) OVER()) AS Churn_Percentage
+FROM churn
+WHERE Customer_Status = 'Churned'
+GROUP BY Churn_Category
+ORDER BY Churn_Percentage DESC;
+
+
+-- 4. What service did churned customers use?
+SELECT Internet_Type, COUNT(Customer_ID) AS Churned, ROUND((COUNT(Customer_ID) * 100.0) / SUM(COUNT(Customer_ID)) OVER(), 1) AS Churn_Percentage
+FROM churn
+WHERE Churn_Category = 'Competitor' 
+AND Customer_Status = 'Churned'
+GROUP BY Internet_Type
+ORDER BY Churned DESC;
+
+-- 5. What contract were churners on?
+SELECT Contract, COUNT(Customer_ID) AS Churned, ROUND(COUNT(Customer_ID) * 100.0 / SUM(COUNT(Customer_ID)) OVER(), 1) AS Churn_Percentage
+FROM  churn
+WHERE Customer_Status = 'Churned'
+GROUP BY Contract
+ORDER BY Churned DESC;
+
+-- 6. Did churners have premium tech support?
+SELECT Premium_Tech_Support, COUNT(Customer_ID) AS Churned, ROUND(COUNT(Customer_ID) *100.0 / SUM(COUNT(Customer_ID)) OVER(),1) AS Churn_Percentage
+FROM churn
+WHERE Customer_Status = 'Churned'
+GROUP BY Premium_Tech_Support
+ORDER BY Churned DESC;
+
+
+-- 7. What Internet service were churners on?
+SELECT Internet_Type, COUNT(Customer_ID) AS Churned, ROUND(COUNT(Customer_ID) * 100.0 / SUM(COUNT(Customer_ID)) OVER(), 1) AS Churn_Percentage
+FROM churn
+WHERE Customer_Status = 'Churned'
+GROUP BY Internet_Type
+ORDER BY Churned DESC;
+
+-- 8. Typical tenure for churners
+SELECT
+    CASE 
+        WHEN Tenure_in_Months <= 6 THEN '6 months'
+        WHEN Tenure_in_Months <= 12 THEN '1 Year'
+        WHEN Tenure_in_Months <= 24 THEN '2 Years'
+        ELSE '> 2 Years'
+    END AS Tenure,
+    COUNT(Customer_ID) AS Churned, CEILING(COUNT(Customer_ID) * 100.0 / SUM(COUNT(Customer_ID)) OVER()) AS Churn_Percentage
+FROM churn
+WHERE Customer_Status = 'Churned'
+GROUP BY
+    CASE 
+        WHEN Tenure_in_Months <= 6 THEN '6 months'
+        WHEN Tenure_in_Months <= 12 THEN '1 Year'
+        WHEN Tenure_in_Months <= 24 THEN '2 Years'
+        ELSE '> 2 Years'
+    END
+ORDER BY
+Churned DESC;
+
+
+-- 9. Are high value customers at risk?
+SELECT 
+    CASE 
+        WHEN (num_conditions >= 3) THEN 'High Risk'
+        WHEN num_conditions = 2 THEN 'Medium Risk'
+        ELSE 'Low Risk'
+    END AS risk_level,
+    COUNT(Customer_ID) AS num_customers, ROUND(COUNT(Customer_ID) *100.0 / SUM(COUNT(Customer_ID)) OVER(),1) AS cust_percentage, num_conditions  
+FROM 
+    (
+    SELECT 
+        Customer_ID,
+        SUM(CASE WHEN Offer = 'Offer E' OR Offer = 'None' THEN 1 ELSE 0 END)+
+        SUM(CASE WHEN Contract = 'Month-to-Month' THEN 1 ELSE 0 END) +
+        SUM(CASE WHEN Premium_Tech_Support = 'No' THEN 1 ELSE 0 END) +
+        SUM(CASE WHEN Internet_Type = 'Fiber Optic' THEN 1 ELSE 0 END) AS num_conditions
+    FROM churn
+    WHERE 
+        Monthly_Charge > 70.05 
+        AND Customer_Status = 'Stayed'
+        AND Number_of_Referrals > 0
+        AND Tenure_in_Months > 9
+    GROUP BY Customer_ID
+    HAVING 
+        SUM(CASE WHEN Offer = 'Offer E' OR Offer = 'None' THEN 1 ELSE 0 END) +
+        SUM(CASE WHEN Contract = 'Month-to-Month' THEN 1 ELSE 0 END) +
+        SUM(CASE WHEN Premium_Tech_Support = 'No' THEN 1 ELSE 0 END) +
+        SUM(CASE WHEN Internet_Type = 'Fiber Optic' THEN 1 ELSE 0 END) >= 1
+    ) AS subquery
+GROUP BY 
+    CASE 
+        WHEN (num_conditions >= 3) THEN 'High Risk'
+        WHEN num_conditions = 2 THEN 'Medium Risk'
+        ELSE 'Low Risk'
+    END, num_conditions; 
+
+
+-- 10. Why did customer's churn exactly?
+SELECT Churn_Reason, Churn_Category, ROUND(COUNT(Customer_ID) *100 / SUM(COUNT(Customer_ID)) OVER(), 1) AS churn_perc
+FROM churn
+WHERE Customer_Status = 'Churned'
+GROUP BY Churn_Reason, Churn_Category
+ORDER BY churn_perc DESC
+LIMIT 10;
+
+
+-- 11. What offers were churners on?
+SELECT  Offer, ROUND(COUNT(Customer_ID) * 100.0 / SUM(COUNT(Customer_ID)) OVER(), 1) AS Churn_Percentage
+FROM churn
+WHERE Customer_Status = 'Churned'
+GROUP BY Offer
+ORDER BY churn_percentage DESC;
+
+
+-- 12. How old were churners?
+SELECT  
+    CASE
+        WHEN Age <= 30 THEN '19 - 30 yrs'
+        WHEN Age <= 40 THEN '31 - 40 yrs'
+        WHEN Age <= 50 THEN '41 - 50 yrs'
+        WHEN Age <= 60 THEN '51 - 60 yrs'
+        ELSE  '> 60 yrs'
+    END AS Age,
+    ROUND(COUNT(Customer_ID) * 100 / SUM(COUNT(Customer_ID)) OVER(), 1) AS Churn_Percentage
+FROM churn
+WHERE Customer_Status = 'Churned'
+GROUP BY
+    CASE
+        WHEN Age <= 30 THEN '19 - 30 yrs'
+        WHEN Age <= 40 THEN '31 - 40 yrs'
+        WHEN Age <= 50 THEN '41 - 50 yrs'
+        WHEN Age <= 60 THEN '51 - 60 yrs'
+        ELSE  '> 60 yrs'
+    END
+ORDER BY
+Churn_Percentage DESC;
